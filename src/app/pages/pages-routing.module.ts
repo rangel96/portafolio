9f3d60dc7389c183462dec99bbc9811de0e02f72
Eric Rangel Carmona <dreamereric28@gmail.com>
2021-05-04 08:20:26 -0500

    Estructura del sidebar

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+];
+
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  imports: [RouterModule.forChild(routes)],
+})
+export class PagesRoutingModule {}
